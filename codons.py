6fb5981e0bf65e8ff5bebee10b1c5cae9c765e1a
OrGutman <or.gutman7@gmail.com>
2025-11-27 10:58:36 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,13 @@
-def create_codon_dict(file_path):
-    pass # Replace the pass with your code
-
-
+def create_codon_dict(path="/content/data/codons.txt"):
+    file = open(path)
+    rows = file.readlines()
+    
+    codon_dict = {}
+    for row in rows:
+        parts = row.strip().split('\t')
+        codon = parts[0]
+        amino_acid = parts[2]
+        codon_dict[codon] = amino_acid
+    
+    file.close()
+    return codon_dict
